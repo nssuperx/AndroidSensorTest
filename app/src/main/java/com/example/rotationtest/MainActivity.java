413fa31f78977a2ff5e6a83314c6cee255924867
nssuperx <mgtmgtx@gmail.com>
2020-05-08 00:29:42 +0900

    hello world

---
+++ app/src/main/java/com/example/rotationtest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.rotationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textview = findViewById(R.id.textView);
+        textview.setText(R.string.text);
+    }
+}
